d287ecf7ff5a45b7adffe5cf15ed59b66e5aca9c
Alexey Hromov <bratpidd2012@gmail.com>
2024-08-31 20:06:43 +0300

    updated backend url

--- src/Client.js
+++ src/Client.js
@@ -3,8 +3,8 @@ import mime from 'rest/interceptor/mime';
 
 class Client {
     constructor() {
-        // this.back_url = "http://92.53.77.158:9000";
-        this.back_url = "http://localhost:9000";
+        this.back_url = "http://92.53.77.158:9000";
+        // this.back_url = "http://localhost:9000";
         this.client = rest.wrap(mime);
     }
 
